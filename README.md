7472c8adaa546fa929b09619784faa4cb0f980d4
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 16:41:48 +0700

    js lab 41

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # js-lab-41
 ### Lab41 Loop: Lab2
+code ทั้งสองอันนี้จะแสดง alert ออกมาเหมือนกันทั้งหมดหรือไม่
+
+let i = 0;
+while (++i < 5) alert( i );
+
+
+let i = 0;
+while (i++ < 5) alert( i );
--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+
+let i = 0;
+while (++i < 5) console.log( i );   //1, 2, 3, 4
+// ++i increment value 1 first then return value
+
+
+let i = 0;
+console.log("value of i: ", i)
+while (i++ < 5) console.log( i );   //1, 2, 3, 4, 5
+//i++  return current value then increment 1 
